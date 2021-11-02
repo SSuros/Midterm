59c34854f761699f28049ee0bb191f14d8d5b786
Brad <90672862+SSuros@users.noreply.github.com>
2021-11-02 04:14:33 -0400

    Made a fully working version with different menus

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene("SpeedRun");
+    }
+
+}
